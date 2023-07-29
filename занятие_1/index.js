6738ab32e64a5c47f9902b165ff938eb5e5ac66e
buldygina <125988250+buldygina@users.noreply.github.com>
2023-07-30 00:14:59 +0300

    Update index.js

--- занятие_1/index.js
+++ занятие_1/index.js
@@ -6,3 +6,12 @@ console.log(nums[0])
     console.log(nums[4])
     console.log(nums[7])
 }
+// задание 2
+let nums = [-10, -3, -1, 1, 6, 18]
+for (let i in nums){
+   nums[i] = nums[i] * 2
+}
+for (let i= 0; i >= 6; i++){
+    nums.push(i)
+}
+console.log(nums)
