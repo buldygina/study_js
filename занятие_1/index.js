c4a22aa6f831ade4f041177a8ab679e570d46ed7
buldygina <125988250+buldygina@users.noreply.github.com>
2023-07-29 23:51:20 +0300

    Update index.js

--- занятие_1/index.js
+++ занятие_1/index.js
@@ -1,7 +1,8 @@
 // задание 1
- let nums = [15, 24312, 732812, 347193, 33, 51, 22, 123, 653, 284723894729]
-for (i = 0; i === 2;) {
-  nums.push(i)
-  i++
+let nums  = [15, 24312, 732812, 347193, 33, 51, 22, 123, 653, 284723894729]
+for (let i = 0; i<= 10; i ++){
+    nums.push(i)
+console.log(nums[0])
+    console.log(nums[4])
+    console.log(nums[7])
 }
-console.log(nums.length[i])
