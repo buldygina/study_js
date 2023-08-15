d7f9fe7840ca1bc406457351082dc1887f5fed54
buldygina <el.buldygina@mail.ru>
2023-08-15 20:47:01 +0300

    bubble sort Co-authored-by: S1rok1 <s1r0k.dev@gmail.com>

--- занятие_1/index.js
+++ занятие_1/index.js
@@ -1,7 +1,46 @@
 // задание 1
- let nums = [15, 24312, 732812, 347193, 33, 51, 22, 123, 653, 284723894729]
-for (i = 0; i === 2;) {
-  nums.push(i)
-  i++
+// let nums  = [15, 24312, 732812, 347193, 33, 51, 22, 123, 653, 284723894729]
+//     console.log(nums)
+//     console.log(nums[0])
+//     console.log(nums[4])
+//     console.log(nums[7])
+
+let nums = [-10, -3, -1, 1, 18, 6]
+for ( let i =0; i < nums.length; i++){
+    nums[i] = nums[i] * nums[i]
 }
-console.log(nums.length[i])
+console.log(nums)
+let swapped
+let sortCount = nums.length - 1
+do {
+    swapped = false
+    for (let i = 0; i < sortCount; i ++){
+        if (nums[i] > nums[i + 1]){
+            console.log('сравниваю', nums[i], 'с ', nums[i + 1])
+            let a = nums[i]
+            nums[i] = nums[i + 1]
+            nums[i + 1] = a
+            swapped = true
+        }
+    
+    }
+    sortCount --
+}
+while(swapped)
+console.log(nums)
+// 3 задание
+// let nums = [1, 1, 1, 1, 1, 1, 1, 1, 0,  0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1]
+// let a = 0
+// let max = 0 
+// for ( let i = 0; i < nums.length; i ++){
+//     if (nums[i] === 1){
+//         a++
+//     }
+//     else{
+//         if (max < a){
+//             max = a
+//         }
+//         a = 0
+//     }
+// }
+// console.log('макс-ное кол-во единиц', max)
---
+++ занятие_1/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+console.log(nums)
+// for (i = 0; i < 6; i++){
+// nums[i] * nums[i]
+// }
+// console.log(i)
