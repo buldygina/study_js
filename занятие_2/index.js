4364b34e90430e35501c040e89857ad74720d50b
S1rok1 <s1r0k.dev@gmail.com>
2023-08-20 22:24:25 +0300

    objects

--- занятие_2/index.js
+++ занятие_2/index.js
@@ -1,12 +1,103 @@
+const process = require("node:process")
 //Объекты
 //Объект выглядит так
+//js
 let object = {
-    string: "bar",
+    stroka: "bar",
     number: 0,
-    array: [0, 1, 2, 3],
+    massiv: [0, 1, 2, 3],
     stringArray: ["s", "o", "m", "e", "t", "h", "i", "n", "g"],
     objectInObject: {
         anotherProps: ""
+    },
+    myFunction: function(){
+        console.log("hello world")
     }
+}
+
+console.log(object["stringArray"][2])
+console.log(object.stringArray[2])
+object.myFunction()
+
+
+let users = [
+    {
+        name: "sasha",
+        age: 19
+    },
+    {
+        name: "liza",
+        age: 19
+    }
+]
+
+for (let person of users){
+    console.log(person.age)
+}
+// базa данных
+let socialNetworkDataBase = {
+    users: [],
+    groups: [],
+    clips: [],
+    music: [],
+    stickers: []
+}
+
+function addUser(phone, email, password){
+    socialNetworkDataBase.users.push({
+        phoneNumber: phone,
+        email: email,
+        password: password,
+        registredAt: "20.08.23",
+        avatar: "pic",
+        userStickers: [],
+        userMusic: [],
+        userClips: [],
+        addusersticker: function(emoji, numb){
+            for (let user of socialNetworkDataBase.users){
+                if (numb === user.phoneNumber){
+                    user.userStickers.push(emoji)
+
+                }
+            }
+        }
+
+    })
+}
+socialNetworkDataBase.users[0].addusersticker("😒", socialNetworkDataBase.users[0].phoneN)
+function newGroup(name, descrition, avatar){
+    socialNetworkDataBase.groups.push({
+        name: name,
+        descr: descrition,
+        avatar: avatar,
+        registredAt: "20.08.23",
+        users: []
+    })
+}
+
+function addClip(shortvideo, description, user){
+    socialNetworkDataBase.clips.push({
+        video: shortvideo,
+        opisanie: description,
+        user: user,
+    })
+}
+function addSticker(emoji){
+    socialNetworkDataBase.stickers.push({
+        emoji: emoji
+    })
     
 }
+function addSticker(file, name){
+    socialNetworkDataBase.stickers.push({
+        file: file,
+        name: name
+    })
+    
+}
+
+// ввод данных
+addUser("89160446717", "example@example.com", "hiokko")
+console.log(socialNetworkDataBase)
+
+
